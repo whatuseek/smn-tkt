5073ba523e8ed1a8d899ebc4f223a366915f466a
Thaika <mytimepasspass@gmail.com>
2025-04-22 17:11:17 +0530

    for deployment purpose

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,7 +39,7 @@ app.use(errorHandler);
 // --- Server ---
 const PORT = process.env.PORT || 5000; // Use port from .env or default to 5000
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`--------------------------------------------------`);
     console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
     console.log(`--------------------------------------------------`);
